08c9f866fca6f0fd9ea5bba3293f9980c63941cf
cmurestudillos <39341418+cmurestudillos@users.noreply.github.com>
2020-04-16 08:59:35 +0200

    Add files via upload

---
+++ src/app/tabs/tabs.module.ts
@@ -0,0 +1,19 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+// Rutas
+import { TabsPageRoutingModule } from './tabs-routing.module';
+// Paginas
+import { TabsPage } from './tabs.page';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    TabsPageRoutingModule
+  ],
+  declarations: [TabsPage]
+})
+export class TabsPageModule {}
